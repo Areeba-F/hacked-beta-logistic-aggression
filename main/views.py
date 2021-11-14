3d6cface31d232d0de5df9c274e05abe051702e9
Nicholas Almeida <n.almeida330@gmail.com>
2021-11-13 17:52:06 -0700

    added image folder

--- main/views.py
+++ main/views.py
@@ -1,19 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext
+from .forms import MedicalForm
 
 # Create your views here.
 def say_hello(request):
+    forms = MedicalForm()
     x = 1
-    return render(request, 'hello.html', {'name': x })
+    return render(request, 'hello.html', {'name': x }), {'forms': forms}
 
 
 def runtest(request): 
+    name = myForm.cleaned_data['name']
     results = False
     if (results == True):
         return render(request, 'positive.html')
     else:
-        return render(request, 'negative.html')
+        return render(request, 'negative.html', {'name': name })
 
 def test():
     return "test"
