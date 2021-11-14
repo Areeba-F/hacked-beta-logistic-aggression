39cf59c99bdfd9df04d5ad9d4f46e242928dc1b8
Nicholas Almeida <n.almeida330@gmail.com>
2021-11-13 17:09:59 -0700

    form added to main page

--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext
 
 # Create your views here.
 def say_hello(request):
@@ -10,7 +11,7 @@ def say_hello(request):
 def runtest(request): 
     results = False
     if (results == True):
-        return render(request, 'positive.html',)
+        return render(request, 'positive.html')
     else:
         return render(request, 'negative.html')
 
@@ -18,3 +19,4 @@ def test():
     return "test"
 
 
+# need to clean age, no days
