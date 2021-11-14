9c622120415eb4588107b9c105803b260e73e813
Nicholas Almeida <n.almeida330@gmail.com>
2021-11-13 20:35:30 -0700

    MLengine implemented and tested.

--- main/views.py
+++ main/views.py
@@ -11,11 +11,12 @@ def say_hello(request):
 def runtest(request): 
     ## initialize variables from form
     name = request.POST['your_name']
-    age = request.POST['your_age']
-    height = request.POST['your_height']
-    weight = request.POST['your_weight']
-    sbp = request.POST['your_sbp']
-    dbp = request.POST['your_dbp']
+    age = int(request.POST['your_age'])
+    age = age * 365
+    height = int(request.POST['your_height'])
+    weight = int(request.POST['your_weight'])
+    sbp = int(request.POST['your_sbp'])
+    dbp = int(request.POST['your_dbp'])
 
     ## intialize variables from radio buttons
     gender = request.POST['gender']
@@ -25,17 +26,18 @@ def runtest(request):
         gender = 1
 
     cholestoral = request.POST['cholestoral']
-    if(cholestoral == 'Normal'):
+
+    if(cholestoral == 'norm'):
         cholestoral = 1
-    elif(cholestoral == 'Above Normal'):
+    elif(cholestoral == 'abvnorm'):
         cholestoral = 2
     else:
         cholestoral = 3
 
     glucose = request.POST['cholestoral']
-    if(glucose == 'Normal'):
+    if(glucose == 'norm'):
         glucose = 1
-    elif(glucose == 'Above Normal'):
+    elif(glucose == 'abvnorm'):
         glucose = 2
     else:
         glucose = 3
@@ -56,20 +58,48 @@ def runtest(request):
     if(exercise == 'Yes'):
         exercise = 1
     else:
-        exercise = 0
-
-
-    ## get results from Machine Learning model
-    ##result = predict(name, age, height, weight, sbp, dbp, gender, cholestoral, glucose, smoke, drink, exercise)
-
-    results = False
+        exercise = 0 
+         
+    results = MLEngine(age, gender, height, weight, sbp, dbp, cholestoral, glucose, smoke, drink, exercise) 
     if (results == True):
         return render(request, 'positive.html')
     else:
         return render(request, 'negative.html', {'name': gender })
 
-def test():
-    return "test"
+def MLEngine(age, gender, height, weight, sbp, dbp, cholestoral, glucose, smoke, drink, exercise):
+    import pandas as pandas
+    import numpy as numpy
+    import matplotlib.pyplot as plot
+    print(age, gender, height, weight, sbp, dbp, cholestoral, glucose, smoke, drink, exercise)
 
 
-# need to clean age, no dayst
+
+    Data = pandas.read_csv('Rcardiotrain.csv')
+    x = Data.iloc[:, :-1]
+    y = Data.iloc[:, -1]
+
+    from sklearn.model_selection import train_test_split
+    x_training, x_testing, y_training, y_testing = train_test_split(x, y, test_size = 0.20, random_state = 42)
+
+    from sklearn.preprocessing import StandardScaler
+    scaler = StandardScaler()
+    x_training = scaler.fit_transform(x_training)
+    x_testing = scaler.transform(x_testing)
+
+    from sklearn.linear_model import LogisticRegression
+    model = LogisticRegression()
+    model.fit(x_training, y_training) # we fit/train/teach the model with our training data
+    predicted_results = model.predict(x_testing)
+
+    from sklearn.metrics import accuracy_score
+    accuracy_score(y_testing, predicted_results)
+
+    result = model.predict(scaler.transform([[age, gender, height, weight, sbp, dbp, cholestoral, glucose, smoke, drink, exercise]]))
+    print(result)
+    if result == 0:
+        return False
+    elif result ==1:
+        return True
+
+
+# need to clean age, no dayst  
